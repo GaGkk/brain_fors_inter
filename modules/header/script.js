82fc29c9575cb523f31d3877c217e1c64cec4881
Garik <qgagk99@gmail.com>
2025-02-17 18:10:06 +0400

    added header module

--- modules/header/script.js
+++ modules/header/script.js
@@ -1,26 +1,15 @@
 const navigation = [
-    {
-        id:1,
-        slug:'about_us',
-        text:'О НАС',
-        link:''
-    },
-    {
-        id:2,
-        slug:'services',
-        text:'УСЛУГИ',
-        link:''
-    },
-    {
-        id:3,
-        slug:'projects',
-        text:'ПРЕКТЫ',
-        link:''
-    },
-    {
-        id:4,
-        slug:'contacts',
-        text:'КОНТАКТЫ',
-        link:''
-    },
-]
+    { id: 1, slug: "about_us", text: "О НАС", link: "../../pages/about-us/about-us.html" },
+    { id: 2, slug: "services", text: "УСЛУГИ", link: "../../pages/services/services.html" },
+    { id: 3, slug: "projects", text: "ПРОЕКТЫ", link: "../../pages/projects/projects.html" },
+    { id: 4, slug: "contacts", text: "КОНТАКТЫ", link: "../../pages/contacts/contacts.html" },
+];
+
+document.addEventListener("DOMContentLoaded", function () {
+    const navElement = document.querySelector(".nav");
+    if (navElement) {
+        navElement.innerHTML = navigation
+            .map(item => `<a href="${item.link}">${item.text}</a>`)
+            .join("");
+    }
+});
