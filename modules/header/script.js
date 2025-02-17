0fd5544ae200982134d9b20c2cb30b9c56a07b57
Garik <qgagk99@gmail.com>
2025-02-17 17:49:44 +0400

    create project and start creating modules

---
+++ modules/header/script.js
@@ -0,0 +1,26 @@
+const navigation = [
+    {
+        id:1,
+        slug:'about_us',
+        text:'О НАС',
+        link:''
+    },
+    {
+        id:2,
+        slug:'services',
+        text:'УСЛУГИ',
+        link:''
+    },
+    {
+        id:3,
+        slug:'projects',
+        text:'ПРЕКТЫ',
+        link:''
+    },
+    {
+        id:4,
+        slug:'contacts',
+        text:'КОНТАКТЫ',
+        link:''
+    },
+]
