52f6d895fb0819781cb2cbd72eab4537906ffd91
Garik <qgagk99@gmail.com>
2025-02-18 18:28:41 +0400

    added about-us page

--- pages/about-us/script.js
+++ pages/about-us/script.js
@@ -1,8 +1,104 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("module.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("content").innerHTML = data;
-        })
-        .catch(error => console.error("Error loading module:", error));
-});
+
+const content = [
+    {
+        type: 'left-text-image',
+        title: 'О НАС',
+        text: [
+            `
+            Принимая во внимание показатели успешности, глубокий 
+            уровень погружения требует анализа благоприятных перспектив. Предварительные выводы неутешительны: консультация с широким активом играет важную роль в формировании экспериментов, поражающих по своей масштабности и грандиозности. 
+            Для современного мира укрепление и развитие внутренней 
+            структуры не оставляет шанса для переосмысления внешнеэкономических политик.
+            `,
+            `
+            Принимая во внимание показатели успешности, глубокий уровень погружения требует анализа благоприятных перспектив. Предварительные выводы неутешительны: консультация с широким активом играет важную роль в формировании экспериментов, поражающих по своей масштабности и грандиозности. 
+            Для современного мира укрепление и развитие внутренней 
+            структуры не оставляет шанса для переосмысления внешнеэкономических политик.
+            `,
+            `
+            Принимая во внимание показатели успешности, глубокий уровень погружения требует анализа благоприятных перспектив. Предварительные выводы неутешительны: консультация с широким активом играет важную роль в формировании экспериментов, поражающих по своей масштабности и грандиозности. 
+            Для современного мира укрепление и развитие
+            `
+        ],
+        img: '../../assets/images/page_content/about-us_1.png',
+        img_class: 'img_1'
+    },
+    {
+        type: 'right-text-image',
+        title: null,
+        text: [
+            `
+              Принимая во внимание показатели успешности, глубокий уровень
+              погружения требует анализа благоприятных перспектив.
+              Предварительные выводы неутешительны: консультация с широким
+              активом играет важную роль в формировании экспериментов,
+              поражающих по своей масштабности и грандиозности. Для современного
+              мира укрепление и развитие внутренней структуры не оставляет шанса
+              для переосмысления внешнеэкономических политик.
+            `,
+            `
+              Принимая во внимание показатели успешности, глубокий уровень
+              погружения требует анализа благоприятных перспектив.
+              Предварительные выводы неутешительны: консультация с широким
+              активом играет важную роль в формировании экспериментов,
+              поражающих по своей масштабности и грандиозности.
+            `,
+        ],
+        img: '../../assets/images/page_content/about-us_1.png',
+        img_class: 'img_2'
+    },
+
+]
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const decorationsElement = document.querySelector(".content_container")
+//     if (decorationsElement) {
+//         const htmls = ``
+//         decorationsElement.innerHTML = content.map(el => {
+//             switch (el.type) {
+//                 case 'left-text-image':
+//                     htmls.concat(`
+//                         <div class="content_block ${el.type}">
+//                         <div class="text_container">
+//                     `)
+//                     if (el.title) {
+//                         htmls.concat(`
+//                                 <p class="title">
+//                                 ${el.title}
+//                                 </p>
+//                             `)
+//                     }
+//                     for (let i = 0; i < el.text.length; i++) {
+//                         htmls.concat(`
+//                                     <p class="text">${el.text[i]}</p>
+//                                 `)
+//                     }
+//                     htmls.concat(`
+//                         </div>
+//                         <div class="img_container">
+//                             <img class="${el.img_class}" src="${el.img}" alt="">
+//                         </div>
+//                         </div>
+//                     `)
+//                     break
+//                 default:
+//                     console.log('Page creating Error');
+//                     break
+
+
+//             }
+//         }).join("")
+//     }
+// })
+
+// /* <div class="content_block left-text-image">
+// <div class="text_container">
+//         <p class="title">
+//             {TITLE_VARIABLE}
+//         </p>
+//         <p class="text">{VARIABLE}</p>
+// </div>
+// <div class="img_container">
+//     <img class="img_1" src="../../assets/images/page_content/about-us_1.png" alt="">
+// </div>
+// </div> */
