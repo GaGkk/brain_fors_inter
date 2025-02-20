4576487eaa054c0f205191d3ec5b2fb600e471ab
Garik <qgagk99@gmail.com>
2025-02-20 18:38:59 +0400

    finished services

--- pages/services/script.js
+++ pages/services/script.js
@@ -1,13 +1,44 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     fetch('../../modules/header/header.html')
-//     .then(response => {
-//         if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
-//         return response.text();
-//     })
-//     .then(data => {
-//         let splited = data.split("<body>")
-//         splited = splited[1].split("</body>")        
-//         document.querySelector('header').innerHTML = splited[0];
-//     })
-//     .catch(error => console.error('Error loading header:', error));
-// });
+const navigation = [
+    { id: 1, slug: "about_us", text: "О НАС", link: "../../pages/about-us/about-us.html" },
+    { id: 2, slug: "services", text: "УСЛУГИ", link: "../../pages/services/services.html" },
+    { id: 3, slug: "projects", text: "ПРОЕКТЫ", link: "../../pages/projects/projects.html" },
+    { id: 4, slug: "contacts", text: "КОНТАКТЫ", link: "../../pages/contacts/contacts.html" },
+];
+
+const socials = [
+    { id: 1, description: "facebook", link: "https://facebook.com", icon: "../../assets/images/socials/FB.svg" },
+    { id: 2, description: "instagram", link: "https://www.instagram.com", icon: "../../assets/images/socials/INST.svg" },
+    { id: 3, description: "vkontakte", link: "https://vk.com", icon: "../../assets/images/socials/VK.svg" },
+    { id: 4, description: "whatsapp", link: "https://www.whatsapp.com", icon: "../../assets/images/socials/WAPP.svg" },
+];
+
+
+function headerCall(){
+    const navElement = document.querySelector(".header-nav");
+    if (navElement) {
+        navElement.innerHTML = navigation
+            .map(item => `<a href="${item.link}">${item.text}</a>`)
+            .join("");
+    }
+}
+
+function footerCall(){
+    const navElement = document.querySelector(".footer-nav");
+    const navSocialsElement = document.querySelector(".footer-socials_nav");
+    if (navElement) {
+        navElement.innerHTML = navigation
+            .map(item => `<a href="${item.link}" class="footer-nav_button">${item.text}</a>`)
+            .join("");
+    }
+    if (navSocialsElement) {
+        navSocialsElement.innerHTML = socials
+            .map(item => `<a href="${item.link}" class="footer-socials_logo" style="background-image: url('${item.icon}');"></a>`)
+            .join("");
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+headerCall()
+footerCall()
+});
