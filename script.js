06ac36d9d13f017ac8e54ea05f04110785dbacea
Garik <qgagk99@gmail.com>
2025-02-21 14:33:17 +0400

    starts creating landing page

--- script.js
+++ script.js
@@ -30,3 +30,49 @@
 //     }
 // });
 // And on this part i just surrended :)
+
+const navigation = [
+    { id: 1, slug: "about_us", text: "О НАС", link: "./pages/about-us/about-us.html" },
+    { id: 2, slug: "services", text: "УСЛУГИ", link: "./pages/services/services.html" },
+    { id: 3, slug: "projects", text: "ПРОЕКТЫ", link: "./pages/projects/projects.html" }, 
+    { id: 4, slug: "news", text: "НОВОСТИ", link: "./pages/news/news.html" },
+    { id: 5, slug: "contacts", text: "КОНТАКТЫ", link: "./pages/contacts/contacts.html" },
+];
+
+const socials = [
+    { id: 1, description: "facebook", link: "https://facebook.com", icon: "./assets/images/socials/FB.svg" },
+    { id: 2, description: "instagram", link: "https://www.instagram.com", icon: "./assets/images/socials/INST.svg" },
+    { id: 3, description: "vkontakte", link: "https://vk.com", icon: "./assets/images/socials/VK.svg" },
+    { id: 4, description: "whatsapp", link: "https://www.whatsapp.com", icon: "./assets/images/socials/WAPP.svg" },
+];
+
+
+function headerCall() {
+    const navElement = document.querySelector(".header-nav");
+    if (navElement) {
+        navElement.innerHTML = navigation
+            .map(item => `<a href="${item.link}">${item.text}</a>`)
+            .join("");
+    }
+}
+
+function footerCall() {
+    const navElement = document.querySelector(".footer-nav");
+    const navSocialsElement = document.querySelector(".footer-socials_nav");
+    if (navElement) {
+        navElement.innerHTML = navigation
+            .map(item => `<a href="${item.link}" class="footer-nav_button">${item.text}</a>`)
+            .join("");
+    }
+    if (navSocialsElement) {
+        navSocialsElement.innerHTML = socials
+            .map(item => `<a href="${item.link}" class="footer-socials_logo" style="background-image: url('${item.icon}');"></a>`)
+            .join("");
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    headerCall()
+    footerCall()
+});
