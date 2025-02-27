d4118d5cfb05ff944df5d86af16c2e3556cab006
Garik <qgagk99@gmail.com>
2025-02-27 18:50:59 +0400

    edited projects and make it responsive

--- pages/projects/script.js
+++ pages/projects/script.js
@@ -1,7 +1,8 @@
+
 const navigation = [
     { id: 1, slug: "about_us", text: "О НАС", link: "../../pages/about-us/about-us.html" },
     { id: 2, slug: "services", text: "УСЛУГИ", link: "../../pages/services/services.html" },
-    { id: 3, slug: "projects", text: "ПРОЕКТЫ", link: "../../pages/projects/projects.html" }, 
+    { id: 3, slug: "projects", text: "ПРОЕКТЫ", link: "../../pages/projects/projects.html" },
     { id: 4, slug: "news", text: "НОВОСТИ", link: "../../pages/news/news.html" },
     { id: 5, slug: "contacts", text: "КОНТАКТЫ", link: "../../pages/contacts/contacts.html" },
 ];
@@ -14,12 +15,42 @@ const socials = [
 ];
 
 
-function headerCall() {
+
+let navActive = false
+
+function navCall() {
     const navElement = document.querySelector(".header-nav");
     if (navElement) {
         navElement.innerHTML = navigation
             .map(item => `<a href="${item.link}">${item.text}</a>`)
             .join("");
+    }
+}
+
+function navButtonClick() {
+    navButtonActive()
+    navActive = !navActive
+}
+
+function navButtonActive() {
+    if (navActive) {
+        const body_background = document.querySelector(".body_background");
+        body_background.innerHTML = ``;
+        const header_nav = document.querySelector(".header-nav")
+        header_nav.innerHTML = ``
+        navButton()
+    }
+    else {
+        const body_background = document.querySelector(".body_background");
+        body_background.innerHTML = `<div onclick="navButtonClick()"></div>`
+        navCall()
+    }
+}
+
+function navButton() {
+    const navElement = document.querySelector(".header-nav");
+    if (navElement) {
+        navElement.innerHTML = `<i onclick="navButtonClick()"></i>`
     }
 }
 
@@ -38,8 +69,11 @@ function footerCall() {
     }
 }
 
-
 document.addEventListener("DOMContentLoaded", function () {
-    headerCall()
+    if (window.screen.width >= 1024) {
+        navCall()
+    } else {
+        navButton()
+    }
     footerCall()
 });
